242e92ac408d89217d4ed32276a943953ea9e6ad
Binaya6335 <85494971+Binaya6335@users.noreply.github.com>
2023-08-24 19:46:19 +0530

    Add files via upload

---
+++ screens/IssLocation.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class ISSLocation extends React.Component{
+    render(){
+        return(
+            <Text>ISS Location Screen</Text>
+        )
+    }
+}
